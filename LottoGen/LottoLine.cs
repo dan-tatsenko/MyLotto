def92f23c1330ee73cf36380708ad66119f053be
Dan Tatsenko <danil.tatsenko@gmail.com>
2024-06-11 16:42:57 +1200

    Initial commit. Solution, project, LottoLine class

---
+++ LottoGen/LottoLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoGen
+{
+    internal class LottoLine
+    {
+        int[] numbers = new int[6];
+        public LottoLine()
+        {
+
+        }
+    }
+}
